415f926dedea94d15d3a23d882c122f7c7dc3ec6
Tomas Sardyha <darsain@gmail.com>
2013-12-10 18:07:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# position
+
+Returns element's position box object relative to documentElement.
+
+## Installation
+
+```
+$ component install darsain/position
+```
+
+## Example
+
+```js
+var position = require('position');
+
+console.log(position(element));
+```
+
+Position box object looks like this:
+
+```
+{
+	left: 100,
+	top: 100,
+	width: 200,
+	heigth: 300,
+	right: 300,
+	bottom: 400
+}
+```
+
+You can also pass `window`.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/**
+ * Transport.
+ */
+module.exports = position;
+
+/**
+ * Globals.
+ */
+var win = window;
+var doc = win.document;
+var docEl = doc.documentElement;
+
+/**
+ * Poor man's shallow object extend.
+ *
+ * @param {Object} a
+ * @param {Object} b
+ *
+ * @return {Object}
+ */
+function extend(a, b) {
+	for (var key in b) {
+		a[key] = b[key];
+	}
+	return a;
+}
+
+/**
+ * Checks whether object is window.
+ *
+ * @param {Object} obj
+ *
+ * @return {Boolean}
+ */
+function isWin(obj) {
+	return obj && obj.setInterval != null;
+}
+
+/**
+ * Returns element's object with `left`, `top`, `bottom`, `right`, `width`, and `height`
+ * properties indicating the position and dimensions of element on a page.
+ *
+ * @param {Element} element
+ *
+ * @return {Object}
+ */
+function position(element) {
+	var winTop = win.pageYOffset || docEl.scrollTop;
+	var winLeft = win.pageXOffset || docEl.scrollLeft;
+	var box = { left: 0, right: 0, top: 0, bottom: 0, width: 0, height: 0 };
+
+	if (isWin(element)) {
+		box.width = win.innerWidth || docEl.clientWidth;
+		box.height = win.innerHeight || docEl.clientHeight;
+	} else if (docEl.contains(element) && element.getBoundingClientRect != null) {
+		extend(box, element.getBoundingClientRect());
+		// width & height don't exist in <IE9
+		box.width = box.right - box.left;
+		box.height = box.bottom - box.top;
+	} else {
+		return box;
+	}
+
+	box.top = box.top + winTop - docEl.clientTop;
+	box.left = box.left + winLeft - docEl.clientLeft;
+	box.right = box.left + box.width;
+	box.bottom = box.top + box.height;
+
+	return box;
+}
