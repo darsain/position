6038795adfece4955779519a0273a39c20fcb57f
Tomas Sardyha <darsain@gmail.com>
2015-02-25 18:06:28 +0100

    Rewrite, added relativeTo argument.

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # position
 
-Returns element's position box object relative to documentElement.
+Returns element's position object relative to `document`, `window`, or other elements.
+
+Position object looks like this:
+
+```js
+{
+	left: 100,   // distance from the left side of a relative target
+	top: 100,    // distance from the top side of a relative target
+	width: 200,  // width of element's border-box
+	heigth: 300, // height of element's border-box
+	right: 300,  // left + width
+	bottom: 400  // top + height
+}
+```
+
+###### *Support*
+
+IE8+
 
 ## Installation
 
 ```
-$ component install darsain/position
+component install darsain/position
 ```
 
 ## Example
@@ -13,23 +30,54 @@ $ component install darsain/position
 ```js
 var position = require('position');
 
-console.log(position(element));
+// get position of an `element` relative to `document.documentElement`
+position(element);
+
+// get position of a `window` relative to `document.documentElement` (i.e. viewport position)
+position(window);
+
+// get position of a `document.documentElement` relative to itself
+// `document` is an alias for `document.documentElement`
+// useful for getting the size of a document
+position(document);
+position(document.documentElement);
+
+// get position of an `element` relative to `window`
+position(element, window);
+
+// get position of an `element` relative to other element
+position(element, element.parentNode);
+
+// get position of a `documentElement` relative to `window` or other `element`
+// I don't know why you'd need this, but yeah, it works this way too
+position(document, window);
+position(document.documentElement, window);
+position(document, element);
+position(document.documentElement, element);
 ```
 
-Position box object looks like this:
+## API
 
-```
-{
-	left: 100,
-	top: 100,
-	width: 200,
-	heigth: 300,
-	right: 300,
-	bottom: 400
-}
-```
+### position(element, [relativeTo])
 
-You can also pass `window` to get the current window position on a page.
+Returns position object of an element, or `null` when invalid or detached element has been passed.
+
+**element** and **[relativeTo]** arguments can be any of these:
+
+- `HTMLElement`
+- `window`
+- `document` - alias for `documentElement`
+- `documentElement`
+
+**[relativeTo]** defaults to `documentElement`.
+
+## Quirks
+
+`position()` does what it can to normalize the output between browsers, but there are still some minor inconsistencies.
+
+##### IE8:
+
+- `position(window)` has scrollbars subtracted from its dimensions
 
 ## License
 
--- index.js
+++ index.js
@@ -1,62 +1,83 @@
+var isWindow = require('iswindow');
+
 module.exports = position;
 
-var win = window;
-var doc = win.document;
-var docEl = doc.documentElement;
-
 /**
- * Poor man's shallow object extend.
+ * Poor man's shallow object extend;
  *
- * @param {Object} a
- * @param {Object} b
- *
+ * @param  {Object} a
+ * @param  {Object} b
  * @return {Object}
  */
 function extend(a, b) {
-	for (var key in b) a[key] = b[key];
+	for (var k in b) a[k] = b[k];
 	return a;
 }
 
 /**
- * Checks whether object is window.
- *
- * @param {Object} obj
- *
- * @return {Boolean}
- */
-function isWin(obj) {
-	return obj && obj.setInterval != null;
-}
-
-/**
- * Returns element's object with `left`, `top`, `bottom`, `right`, `width`, and `height`
- * properties indicating the position and dimensions of element on a page.
+ * Returns element's position object with `left`, `top`, `bottom`, `right`,
+ * `width`, and `height` properties indicating the position and dimensions
+ * of element on a page, or relative to other element.
  *
  * @param {Element} element
+ * @param {Element} [relativeTo] Defaults to `document.documentElement`.
  *
- * @return {Object}
+ * @return {Object|null}
  */
-function position(element) {
-	var winTop = win.pageYOffset || docEl.scrollTop;
-	var winLeft = win.pageXOffset || docEl.scrollLeft;
-	var box = { left: 0, right: 0, top: 0, bottom: 0, width: 0, height: 0 };
+function position(element, relativeTo) {
+	var isWin = isWindow(element);
+	var doc = isWin ? element.document : element.ownerDocument || element;
+	var docEl = doc.documentElement;
+	var win = isWindow(relativeTo) ? relativeTo : doc.defaultView || window;
 
-	if (isWin(element)) {
-		box.width = win.innerWidth || docEl.clientWidth;
-		box.height = win.innerHeight || docEl.clientHeight;
-	} else if (docEl.contains(element) && element.getBoundingClientRect != null) {
-		extend(box, element.getBoundingClientRect());
+	// normalize arguments
+	if (element === doc) element = docEl;
+	relativeTo = !relativeTo || relativeTo === doc ? docEl : relativeTo;
+
+	var winTop = (win.pageYOffset || docEl.scrollTop) - docEl.clientTop;
+	var winLeft = (win.pageXOffset || docEl.scrollLeft) - docEl.clientLeft;
+	var box = { top: 0, left: 0 };
+
+	if (isWin) {
+		box.width = box.right = win.innerWidth || docEl.clientWidth;
+		box.height = box.bottom = win.innerHeight || docEl.clientHeight;
+	} else if (element === docEl) {
+		// we need to do  this manually because docEl.getBoundingClientRect
+		// is inconsistent in <IE11
+		box.top = -winTop;
+		box.left = -winLeft;
+		box.width = Math.max(docEl.clientWidth, docEl.scrollWidth);
+		box.height = Math.max(docEl.clientHeight, docEl.scrollHeight);
+		box.right = box.width - winLeft;
+		box.bottom = box.height - winTop;
+	} else if (docEl.contains(element) && element.getBoundingClientRect) {
+		// new object needed because DOMRect properties are read-only
+		box = extend({}, element.getBoundingClientRect());
 		// width & height don't exist in <IE9
 		box.width = box.right - box.left;
 		box.height = box.bottom - box.top;
 	} else {
-		return box;
+		return null;
 	}
 
-	box.top = box.top + winTop - docEl.clientTop;
-	box.left = box.left + winLeft - docEl.clientLeft;
-	box.right = box.left + box.width;
-	box.bottom = box.top + box.height;
+	// current box is already relative to window
+	if (relativeTo === win) return box;
+
+	// add window offsets, making the box relative to documentElement
+	box.top += winTop;
+	box.left += winLeft;
+	box.right += winLeft;
+	box.bottom += winTop;
+
+	// current box is already relative to documentElement
+	if (relativeTo === docEl) return box;
+
+	// subtract position of other element
+	var relBox = position(relativeTo);
+	box.left -= relBox.left;
+	box.right -= relBox.left;
+	box.top -= relBox.top;
+	box.bottom -= relBox.top;
 
 	return box;
 }
